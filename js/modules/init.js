9d175c3d6d2350288effa60a58b57cbdd0684ee5
d-herbert <deidre.m.herbert@gmail.com>
2022-05-13 17:43:55 -0700

    init working, import is not

--- js/modules/init.js
+++ js/modules/init.js
@@ -1,42 +1,21 @@
-// function init() {
-//     // display command menu
-//     console.log('the employee management application')
-//     console.log('-------------------------------------')  
-//     // start with empty array
-//     let arrEmployees = []
-//     // fetch json data using promises
-//     fetch('../data/employees.json')
-//         .then( response => response.json() )
-//         .then( data =>  {
-//             for (let employee of data.employees) {
-//                 arrEmployees.push([`${employee.ID}, ${employee.Name}, ${employee.Ext}, ${employee.Email}, ${employee.Department}`])
-//             }
-//         })
-//         .catch( error => console.log(error.message))
-// }
-
-function init() {
+export function init() {
     // display command menu
     console.log('the employee management application')
     console.log('-------------------------------------')  
     // start with empty array
     let arrEmployees = []
-    // fetch json data using async/await
-    async function fetchUsers() {
-        try {
-            const response = await fetch('../data/employees.json')
-            const employees = await response.json()
-            for (let employee of employees) {
-                document.body.innerHTML += `${employee.Name}<br>`
+    // fetch json data using promises
+    fetch('../data/employees.json')
+        .then( response => response.json() )
+        .then( data =>  {
+            // console.log(data.employees[0].ID)
+            for (let employee of data.employees) {
                 arrEmployees.push([`${employee.ID}, ${employee.Name}, ${employee.Ext}, ${employee.Email}, ${employee.Department}`])
             }
-            console.log(arrEmployees)
-            return arrEmployees
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    fetchUsers()
+        })
+        .catch( error => console.log(error.message))
+    console.log(arrEmployees)
+    return arrEmployees
 }
 
 init()
